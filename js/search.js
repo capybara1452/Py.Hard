9e09884816965b229311c77bf382194a74d7eab9
capybara1452 <capybara1452@gmail.com>
2023-10-16 01:59:44 +0800

    練習

--- js/search.js
+++ js/search.js
@@ -43,3 +43,10 @@
   
   })(document);
 
+
+
+  
+function ShowMeDate() {
+  　var Today=new Date();
+  　alert("今天日期是 " + Today.getFullYear()+ " 年 " + (Today.getMonth()+1) + " 月 " + Today.getDate() + " 日");
+  }
